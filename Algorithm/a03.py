38ff1b2ea21dbd54b72f63658c71ce8b1b71074c
Iyu-desu <145138733+Iyu-desu@users.noreply.github.com>
2024-02-17 20:22:40 +0700

    Update a03.py

--- Algorithm/a03.py
+++ Algorithm/a03.py
@@ -1,9 +1,10 @@
 def splitTheBill(group):
-    total_cost = sum(group.values())
-    avg_cost = total_cost / len(group)
-    result = {name: round(cost - avg_cost, 2) for name, cost in group.items()}
+    total_cost = sum(group.values())    #รวมค่าใช้จ่ายทั้งหมด
+    avg_cost = total_cost / len(group)    #หาค่าใช้จ่ายเฉลี่ย
+    #นำค่าใช้จ่ายของแต่ละคนมาหักกับค่าใช้จ่ายเฉลี่ยแล้วแจกแจงขของแต่ละคนพร้อมแสดงค่าด้วยทศนิยม 2 ตำแหน่ง
+    result = {name: round(cost - avg_cost, 2) for name, cost in group.items()}    
     return result
-#Example
-group = {“A”: 20, “B”: 15, “C”: 10}
+#ตรวจสอบโดยการใส่คำสั่ง 
+group = {“A”: 20, “B”: 15, “C”: 10} 
 result = spiltTheBill(group)
 print(result)
