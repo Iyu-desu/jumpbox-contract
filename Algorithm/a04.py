9001a66f9a3c027f941fc9488f1ffad94f87ff45
Iyu-desu <145138733+Iyu-desu@users.noreply.github.com>
2024-02-17 20:23:03 +0700

    Update a04.py

--- Algorithm/a04.py
+++ Algorithm/a04.py
@@ -1,10 +1,14 @@
 def fibonacci(n):
-    if n == 0:
+    if n == 0:    #ถ้า F ที่ n =0 จะได้ค่า 0
         return 0
-    elif n == 1:
+    elif n == 1:   #ถ้า F ที่ n =1 จะได้ค่า 1
         return 1
     else:
-        return fibonacci(n-1) + fibonacci(n-2)
-n = int(input("Input: "))
-output = fibonacci(n)
-print("Output:", output)
+        return fibonacci(n-1) + fibonacci(n-2)   #ถ้า F ที่ n =1 จะได้ค่า 1
+n = int(input("Input: "))    #รับค่าเป็นจำนวนเต็ม
+if n > 0:    #ถ้าเป็นจำนวนเต็มบวก ให้ใช้ฟังก์ชันคำนวณค่า Fibonacci และแสดงค่า
+    output = fibonacci(n)
+    print("Output:", output) 
+else:    #ถ้าไม่ใช่จำนวนเต็มบวกให้บอกว่าไม่ใช่ แล้วไม่คำนวณ
+    print(“Input is not correct number”)
+
