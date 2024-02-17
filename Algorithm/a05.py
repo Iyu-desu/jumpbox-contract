83ff88688f3806c1083e6b7e6cc3f2f7f901cf9a
Iyu-desu <145138733+Iyu-desu@users.noreply.github.com>
2024-02-17 20:23:21 +0700

    Update a05.py

--- Algorithm/a05.py
+++ Algorithm/a05.py
@@ -1,9 +1,17 @@
 def palindrome(text):
-    if len(text) <= 1:
+    if len(text) <= 1:    #ถ้าความยาวของ text มีแค่ 1 ตัวอักษรหรือไม่มี จะส่งค่า True ออก
         return True
-    return text[0] == text[-1]
+    elif len(text) > 1:    #ถ้าความยาวของ text มีมากกว่า 1 ตัวอักษร จะทำคำสั่งข้างล่างต่อ
+#ตรวจสอบตัวอักษรตัวแรกและตัวสุดท้ายว่าตรงกันหรือไม่ ถ้าตรงกันก็ตรวจสอบตัวที่สองและตัวรองสุดท้ายต่อ ทำไปจนจบที่ตัวกลางของ text 
+#ระหว่างนี้ถ้าเจอตัวที่ไม่ตรงกันจะส่งค่า False ออก แต่ถ้าตรวจสอบจนครบแล้วจะส่งค่า True ออก
+        for i in range(len(text) // 2 + 1):    
+            if text[i] != text[-i + 1]
+                return False
+    return True
+#คำสั่งด้านล่างเป็นการทดสอบว่า text ที่ใส่ไปเป็น Palindrome หรือไม่
 text = input("Enter text: ")
 if palindrome(text):
     print(text, "is a palindrome")
 else:
     print(text, "is not a palindrome")
+
